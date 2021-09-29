d48ee3d0af0d24321d73ff7cf9b21ada10de27cc
Tom Gottschalk <Tom-gottschalk1@web.de>
2021-09-29 20:47:07 +0200

    Initial Commit

---
+++ Shared/GH_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GH_TutorialApp.swift
+//  Shared
+//
+//  Created by Tom Gottschalk on 29.09.21.
+//
+
+import SwiftUI
+
+@main
+struct GH_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
